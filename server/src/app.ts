be829ad62631e438a42a0b87e67e6ecb472244fd
Francis Lagares <francis.lagares@gmail.com>
2021-03-25 16:45:10 +0100

    feat: create apollo server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,10 @@
 import { ApolloServer } from 'apollo-server-express';
 import express, { Application } from 'express';
-import { schema } from './graphql';
+import { typeDefs, resolvers } from './graphql';
 
 const app: Application = express();
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.applyMiddleware({ app, path: '/api' });
 
-export { app };
+export { app, server };
