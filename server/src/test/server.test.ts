47032d0cf699aa2b229e07bbf0f2bbf1f844368f
Francis Lagares <francis.lagares@gmail.com>
2021-03-25 16:31:15 +0100

    test: write apollo server test

--- server/src/test/server.test.ts
+++ server/src/test/server.test.ts
@@ -1,11 +1,56 @@
-import request from 'supertest';
-import { app } from '../app';
+import { gql } from 'apollo-server-express';
+import { createTestClient } from 'apollo-server-testing';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { server } from '../app';
 
-describe('Server', () => {
-  test('server runs without crashing', async () => {
-    const result = await request(app).get('/').send('NodeJS Server');
+const mockedListings = [
+  {
+    id: '001',
+    title: 'Clean and fully furnished apartment. 5 min away from CN Tower',
+  },
+  {
+    id: '002',
+    title: 'Luxurious home with private pool',
+  },
+  {
+    id: '003',
+    title: 'Single bedroom located in the heart of downtown San Fransisco',
+  },
+];
 
-    expect(result.status).toBe(200);
-    expect(result.text).toBe('NodeJS Server');
+const mockedServer = setupServer(
+  rest.get('/api', (req, res, ctx) => {
+    return res(ctx.json({ listings: mockedListings }));
+  }),
+);
+
+beforeAll(() => {
+  mockedServer.listen();
+});
+afterAll(() => {
+  mockedServer.close();
+});
+
+describe('Apollo Server', () => {
+  test('display listings of apartments', async () => {
+    const { query } = createTestClient(server);
+
+    const { data } = await query({
+      query: gql`
+        {
+          listings {
+            id
+            title
+          }
+        }
+      `,
+    });
+
+    expect(data.listings).toHaveLength(3);
+    expect(data.listings[1]).toEqual({
+      id: '002',
+      title: 'Luxurious home with private pool',
+    });
   });
 });
