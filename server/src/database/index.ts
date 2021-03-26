82301889c57137020d29431cc6056fa950986dd4
Francis Lagares <francis.lagares@gmail.com>
2021-03-26 12:55:32 +0100

    feat: add environment variables to port and mongodb connection

---
+++ server/src/database/index.ts
@@ -0,0 +1,20 @@
+import * as dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+import { Database } from '../lib/types';
+
+dotenv.config();
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/tinyhouse?retryWrites=true&w=majority`;
+
+export const connectDB = async (): Promise<Database> => {
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = client.db('tinyhouse');
+
+  return {
+    listings: db.collection('listings'),
+  };
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,26 @@
-import { app } from './app';
+import { ApolloServer } from 'apollo-server-express';
+import * as dotenv from 'dotenv';
+import express, { Application } from 'express';
+import { connectDB } from './database';
+import { typeDefs, resolvers } from './graphql';
 
-const port = 9000;
-app.listen(port, () => console.log(`[node-server]: http://localhost:${port}`));
+dotenv.config();
+
+const mount = async (app: Application): Promise<void> => {
+  const db = await connectDB();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db }),
+  });
+
+  server.applyMiddleware({ app, path: '/api' });
+
+  app.listen(process.env.PORT, () =>
+    console.log(`[node-server]: http://localhost:${process.env.PORT}`),
+  );
+
+  const listings = await db.listings.find({}).toArray();
+};
+
+mount(express());
