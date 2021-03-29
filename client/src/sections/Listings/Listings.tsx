5f1dff55d823f4233ff46325deabb43cca9f659c
Francis Lagares <francis.lagares@gmail.com>
2021-03-29 20:50:18 +0200

    feat: implement function to fetch api data

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { server } from '../../lib/api';
+import { ListingsData } from './types';
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
 
 const Listings = (): JSX.Element => {
-  return <h2>Tinyhouse Listings</h2>;
+  const fetchListings = async () => {
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
+    console.log(data);
+  };
+  return (
+    <>
+      <h2>Tinyhouse Listings</h2>
+      <button onClick={fetchListings}>Query Listings</button>
+    </>
+  );
 };
 
 export default Listings;
