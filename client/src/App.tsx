9d156b076b9427c56505f6191732e09fee8ceffa
Francis Lagares <francis.lagares@gmail.com>
2021-04-09 12:10:09 +0200

    feat: implement login functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from 'react';
 import { Layout } from 'antd';
 import { Switch, Route } from 'react-router-dom';
+import { Viewer } from './lib/types';
 import {
   Home,
   Host,
@@ -12,7 +14,17 @@ import {
 } from './sections';
 import './App.css';
 
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false,
+};
+
 const App = (): JSX.Element => {
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  console.log(viewer);
   return (
     <Layout id='app'>
       <Switch>
@@ -20,7 +32,11 @@ const App = (): JSX.Element => {
         <Route exact path='/host' component={Host} />
         <Route exact path='/listing/:id' component={Listing} />
         <Route exact path='/listings/:location?' component={Listings} />
-        <Route exact path='/login' component={Login} />
+        <Route
+          exact
+          path='/login'
+          render={props => <Login {...props} setViewer={setViewer} />}
+        />
         <Route exact path='/user/:id' component={User} />
         <Route component={NotFound} />
       </Switch>
