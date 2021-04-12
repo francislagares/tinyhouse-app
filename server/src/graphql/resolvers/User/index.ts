169c791d0f2d3bcb43363cb1e9c900789df21b7a
Francis Lagares <francis.lagares@gmail.com>
2021-04-12 19:14:28 +0200

    feat: implement query resolver to fetch user

--- server/src/graphql/resolvers/User/index.ts
+++ server/src/graphql/resolvers/User/index.ts
@@ -1,9 +1,32 @@
 import { IResolvers } from 'apollo-server-express';
+import { Request } from 'express';
+import { Database, User } from '../../../lib/types';
+import { authorize } from '../../../lib/utils';
+import { UserArgs } from './types';
 
 export const userResolvers: IResolvers = {
   Query: {
-    user: () => {
-      return 'Query.user';
+    user: async (
+      _root: undefined,
+      { id }: UserArgs,
+      { db, req }: { db: Database; req: Request },
+    ): Promise<User> => {
+      try {
+        const user = await db.users.findOne({ _id: id });
+
+        if (!user) {
+          throw new Error(`user can't be found`);
+        }
+
+        const viewer = await authorize(db, req);
+        if (viewer && viewer._id === user._id) {
+          user.authorized = true;
+        }
+
+        return user;
+      } catch (error) {
+        throw new Error(`Failed to query user: ${error}`);
+      }
     },
   },
 };
