75cb7a432666698e36758e482918e558baf722b0
Francis Lagares <francis.lagares@gmail.com>
2021-04-15 18:37:41 +0200

    feat: check if viewer is user and connect stripe button

--- client/src/sections/User/index.tsx
+++ client/src/sections/User/index.tsx
@@ -1,5 +1,65 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { Col, Layout, Row } from 'antd';
+import { RouteComponentProps } from 'react-router-dom';
+import { ErrorBanner, PageSkeleton } from '../../lib/components';
+import { USER } from '../../lib/graphql/queries';
+import {
+  User as UserData,
+  UserVariables,
+} from '../../lib/graphql/queries/User/__generated__/User';
+import { Viewer } from '../../lib/types';
+import { UserProfile } from './components';
 
-export const User = (): JSX.Element => {
-  return <div>User</div>;
+interface Props {
+  viewer: Viewer;
+}
+
+interface MatchParams {
+  id: string;
+}
+
+const { Content } = Layout;
+
+export const User = ({
+  viewer,
+  match,
+}: Props & RouteComponentProps<MatchParams>): JSX.Element => {
+  const { data, loading, error } = useQuery<UserData, UserVariables>(USER, {
+    variables: {
+      id: match.params.id,
+    },
+  });
+
+  if (loading) {
+    return (
+      <Content className='user'>
+        <PageSkeleton />
+      </Content>
+    );
+  }
+
+  if (error) {
+    return (
+      <Content className='user'>
+        <ErrorBanner description="This user may not exist or we've encountered an error. Please try again soon." />
+        <PageSkeleton />
+      </Content>
+    );
+  }
+
+  const user = data ? data.user : null;
+  const viewerIsUser = viewer.id === match.params.id;
+
+  const userProfileElement = user ? (
+    <UserProfile user={user} viewerIsUser={viewerIsUser} />
+  ) : null;
+
+  return (
+    <Content className='user'>
+      <Row gutter={12} justify='space-between'>
+        <Col xs={24}>{userProfileElement}</Col>
+      </Row>
+    </Content>
+  );
 };
